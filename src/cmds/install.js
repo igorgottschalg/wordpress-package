d2b5564af3651ed11e1ff19a1c878c4ce4890789
Igor Gottschalg <igor@gottschalg.mobi>
2020-02-09 11:26:16 -0300

    Add publish script

--- src/cmds/install.js
+++ src/cmds/install.js
@@ -10,14 +10,14 @@ exports.handler = function() {
     if (!ReadFile.check()) return;
     let wp = ReadFile.read();
     if (wp.version) {
-        (async () => await exec(`wp core download ${wp.language} --version=${wp.version}`))();
+        (async () => await exec(`wp core download ${wp.language} --version=${wp.version} --allow-root`))();
     }
     if (wp.language) {
-        (async () => await exec(`wp language core activate ${wp.language}`))();
+        (async () => await exec(`wp language core activate ${wp.language} --allow-root`))();
     }
     if (wp.plugins) {
         wp.plugins.every(
-            async plugin => await exec(`wp plugin install ${plugin}`)
+            async plugin => await exec(`wp plugin install ${plugin} --allow-root`)
         );
     }
 };
